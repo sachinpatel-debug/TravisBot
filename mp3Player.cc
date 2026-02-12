23d4efcc466e41277c541503b309713f108f7809
Sachin Patel <sachinp9292@gmail.com>
2026-02-11 21:35:46 -0500

    Add volume control methods to mp3Player

--- mp3Player.cc
+++ mp3Player.cc
@@ -2,7 +2,7 @@
 #include <iostream>
 
 void mp3Player::playTrack(int trackNumber){
-	std::cout<<"Function play track called " <<std::endl;
+	//std::cout<<"Function play track called " <<std::endl;
 	this->cmd = 0x03;
 	uint16_t parameters = (uint16_t)trackNumber;
 	lowParameter = (parameters & 0xFF);
@@ -11,6 +11,16 @@ void mp3Player::playTrack(int trackNumber){
 	calculateChecksum();
 	refreshData();
 	}
+void mp3Player::raiseVolume(){
+	this->cmd = 0x04;
+	calculateChecksum();
+	refreshData();
+}
+void mp3Player::lowerVolume(){
+	this->cmd = 0x05;
+	calculateChecksum();
+	refreshData();
+}
 void mp3Player::calculateChecksum(){
 	checksum = 0xFFFF - (version + length + cmd + feedback + highParameter + lowParameter)+1; 
 	checksumHigh = (checksum >> 8) & 0xFF;
